4547578a7bd1ca8ca98aeb8a3f0ed0ad5752e0f0
Signed Bit <signedbit@users.noreply.github.com>
2015-10-26 06:25:30 -0400

    inner classes are now converted to smali

--- src/org/ollide/java2smali/Class2DexHelper.java
+++ src/org/ollide/java2smali/Class2DexHelper.java
@@ -10,15 +10,15 @@ public class Class2DexHelper {
      * Uses the dx tool from the Android Build Tools (19.0.1) to create
      * a .dex version of a compiled java file (.class)
      *
-     * @param inputClassFilePath full path to the compiled .class file
+     * @param inputClassFilePaths full paths to the compiled .class file
      * @param outputDexPath this will be the dex output file's path and name
      * @throws IOException
      */
-    public static void dexClassFile(String inputClassFilePath, String outputDexPath) throws IOException {
+    public static void dexClassFile(String[] inputClassFilePaths, String outputDexPath) throws IOException {
         Main.Arguments arguments = new Main.Arguments();
         arguments.outName = outputDexPath;
         arguments.strictNameCheck = false;
-        arguments.fileNames = new String[]{inputClassFilePath};
+        arguments.fileNames = inputClassFilePaths;
 
         Main.run(arguments);
     }
--- src/org/ollide/java2smali/GenerateAction.java
+++ src/org/ollide/java2smali/GenerateAction.java
@@ -12,16 +12,17 @@ import com.intellij.openapi.project.Project;
 import com.intellij.openapi.roots.ProjectRootManager;
 import com.intellij.openapi.vfs.LocalFileSystem;
 import com.intellij.openapi.vfs.VirtualFile;
+import com.intellij.psi.PsiClass;
 import com.intellij.psi.PsiFile;
 import com.intellij.psi.PsiJavaFile;
 import com.intellij.psi.PsiManager;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.*;
 
 public class GenerateAction extends AnAction {
 
-    private static final String CLASS_EXTENSION = ".class";
     private static final String DEX_EXTENSION = ".dex";
     private static final String JAVA_EXTENSION = ".java";
     private static final String SMALI_EXTENSION = ".smali";
@@ -76,15 +77,14 @@ public class GenerateAction extends AnAction {
             this.javaFile = file;
         }
 
-        public void finished(boolean b, int i, int i2, CompileContext compileContext) {
-            VirtualFile outputDirectory = compileContext.getModuleOutputDirectory(module);
-            String compileDirPath = outputDirectory.getPath();
-            String compiledFilePath = getCompiledClassFilePath(compileDirPath);
-
-            String dexFile = compiledFilePath + DEX_EXTENSION;
+        public void finished(boolean aborted, int errors, int warnings, CompileContext compileContext) {
+            if (errors > 0) return;
+        
+            String dexFile = javaFile.getVirtualFile().getNameWithoutExtension() + DEX_EXTENSION;
             // CLASS -> DEX
             try {
-                Class2DexHelper.dexClassFile(compiledFilePath, dexFile);
+                String[] compiledFilePaths = getClassFiles(compileContext);
+                Class2DexHelper.dexClassFile(compiledFilePaths, dexFile);
             } catch (IOException e) {
                 e.printStackTrace();
                 return;
@@ -116,19 +116,72 @@ public class GenerateAction extends AnAction {
             openFileDescriptor.navigate(true);
         }
 
-        private String getCompiledClassFilePath(String dirPath) {
-            String packageName = javaFile.getPackageName();
-            String[] packages = packageName.split("\\.");
+        // Mary wishes she had a little lambda
+        private String[] getClassFiles(CompileContext context) {
+            VirtualFile outDir = context.getModuleOutputDirectory(module);
 
-            StringBuilder sb = new StringBuilder(dirPath);
-            for (String p : packages) {
-                sb.append(File.separator);
-                sb.append(p);
+            PsiClass[] classes = javaFile.getClasses();
+            Set<String> classNames = new HashSet<>(classes.length);
+            for (PsiClass c : classes) {
+                classNames.add(c.getName());
             }
-            sb.append(File.separator);
-            sb.append(javaFile.getContainingFile().getVirtualFile().getNameWithoutExtension());
-            sb.append(CLASS_EXTENSION);
-            return sb.toString();
+
+            String pkg = javaFile.getPackageName().replace('.', '/');
+
+            List<VirtualFile> compiledAsList = Arrays.asList(outDir.findFileByRelativePath(pkg).getChildren());
+            List<VirtualFile> compiledChildren = new LinkedList<>(compiledAsList);
+
+            List<VirtualFile> sourceAsList = Arrays.asList(javaFile.getVirtualFile().getParent().getChildren());
+            List<VirtualFile> sourceChildren = new LinkedList<>(sourceAsList);
+
+            for (Iterator<VirtualFile> iterCompiled = compiledChildren.iterator(); iterCompiled.hasNext(); ) {
+                VirtualFile f = iterCompiled.next();
+                String name = f.getNameWithoutExtension();
+                if (f.isDirectory() || !f.getExtension().equals("class")) {
+                    iterCompiled.remove();
+                    continue;
+                }
+
+                boolean b = false;
+                for (String className : classNames) {
+                    if (name.startsWith(className)) {
+                        b = true;
+                        break;
+                    }
+                }
+
+                if (!b) {
+                    // this isn't part of the file we want
+                    iterCompiled.remove();
+                    continue;
+                }
+
+                // remove classes that start with the class name we want, but aren't actually related
+                // example: we have A.java and AB.java, if we j2s A.java then we don't want AB.smali
+                for (Iterator<VirtualFile> iterSource = sourceChildren.iterator(); iterSource.hasNext(); ) {
+                    VirtualFile sVf = iterSource.next();
+                    String sourceName = sVf.getNameWithoutExtension();
+                    if (sVf.isDirectory() || classNames.contains(sourceName)) {
+                        // don't waste my clock cycles in the future
+                        iterSource.remove();
+                        continue;
+                    }
+
+                    if (name.startsWith(sourceName)) {
+                        iterCompiled.remove();
+                        iterSource.remove();
+                        break;
+                    }
+                }
+            }
+
+            int size = compiledChildren.size();
+            String[] compiled = new String[size];
+            VirtualFile[] compiledFiles = compiledChildren.toArray(new VirtualFile[size]);
+            for (int i = 0; i < size; i++) {
+                compiled[i] = compiledFiles[i].getPath();
+            }
+            return compiled;
         }
     }
 }
